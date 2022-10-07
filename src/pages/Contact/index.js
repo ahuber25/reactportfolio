e7c81339fa72eecf932d3e226a0fc0b51cd8a3d8
Scotch <horsinaround235@gmail.com>
2022-10-07 10:54:29 -0400

    added contact form and moved around files again

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,21 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
 
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.')
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value })
+        }
+        console.log(formState);
+        console.log( errorMessage);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
+
         <section class="items" id="contact">
         <div class="section-titles">
             <h3>Contact Me</h3>
         </div>
         <div class="find-me">
-            <ul>
-                <li>215-530-2314</li>
-                <li><a href="mailto:horsinaround235@gmail.com">horsinaround235@gmail.com</a></li>
-                <li><a href="https://github.com/ahuber25" target="_blank" rel="noopener noreferrer">Github</a></li>
-            </ul>
+        <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" defaultValue={name} onChange={handleChange} name="name" />
         </div>
-    </section>
+        <div>
+            <label htmlFor="email">Email address:</label>
+            <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+        </div>
+        <div>
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
+        </div>
+        {errorMessage && (
+            <div>
+                <p className="error-text">{errorMessage}</p>
+            </div>
+        )}
+        <button type="submit">Submit</button>
+        </form>
+        </div>
+        </section>
+
     )
-}
+
+};
 
 export default Contact;
